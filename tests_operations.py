b67591b6018fb4d7824d0d03bc0213383588d10e
Nicholas Choong <nicholascwc1997@gmail.com>
2023-11-24 18:53:45 +0800

    init

---
+++ tests_operations.py
@@ -0,0 +1,52 @@
+from operations import addition, subtraction, multiplication, division
+
+
+def test_addition():
+    """
+    GIVEN: Two numbers
+    WHEN:  passed in to the function addition
+    THEN:  the resulting is the addition of the two numbers
+    """
+    # assert addition(12, 12) == 24
+    pass
+
+
+def test_subtraction():
+    """
+    GIVEN: Two numbers
+    WHEN:  passed in to the function subtraction
+    THEN:  the resulting is the subtraction of the two numbers
+    """
+    # assert subtraction(7, 10) == -3
+    pass
+
+
+def test_multiplication():
+    """
+    GIVEN: Two numbers
+    WHEN:  passed in to the function multiplication
+    THEN:  the resulting is the multiplication of the two numbers
+    """
+    # assert multiplication(3, 5) == 15
+    pass
+
+
+def test_division():
+    """
+    GIVEN: Two numbers
+    WHEN:  passed in to the function division
+    THEN:  the resulting is the division of the two number
+    """
+    # assert division(8, 4) == 2
+    pass
+
+
+def test_division_exception_on_zero():
+    """
+    GIVEN: Two numbers
+    WHEN:  passed in to the function division
+    THEN:  the resulting is the division of the two number
+
+    Hint: https://stackoverflow.com/questions/23337471/how-to-properly-assert-that-an-exception-gets-raised-in-pytest
+    """
+    pass
